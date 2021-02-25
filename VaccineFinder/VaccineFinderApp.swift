2f609c198b0c6c16cfeb6e67de4ef280bdddabcb
Bart Whiteley <601312+bwhiteley@users.noreply.github.com>
2021-02-24 23:05:59 -0700

    Initial Commit

---
+++ VaccineFinder/VaccineFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccineFinderApp.swift
+//  VaccineFinder
+//
+//  Created by Bart Whiteley on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct VaccineFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
