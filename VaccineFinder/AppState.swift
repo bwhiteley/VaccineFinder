6ecbeaabe407582ddf94b8a1587ae717bd1d2bfc
Bart Whiteley <601312+bwhiteley@users.noreply.github.com>
2021-02-25 20:08:48 -0700

    dev checkpoint

---
+++ VaccineFinder/AppState.swift
@@ -0,0 +1,38 @@
+//
+//  AppState.swift
+//  VaccineFinder
+//
+//  Created by Bart Whiteley on 2/24/21.
+//
+
+import Foundation
+import Combine
+
+class AppState: ObservableObject {
+    @Published var events: [String: [VEvent]] = [:]
+    
+    static let shared: AppState = AppState()
+    
+    private init() {}
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func load() {
+        let url = URL(string: "https://healthevents.utahcounty.gov/api/events")!
+        let task = URLSession.shared.dataTaskPublisher(for: url)
+        task.receive(on: DispatchQueue.main).sink { (completion: Subscribers.Completion<URLSession.DataTaskPublisher.Failure>) in
+            
+        } receiveValue: { (output: URLSession.DataTaskPublisher.Output) in
+            guard let events = try? JSONDecoder().decode([String: [VEvent]].self, from: output.data) else { return }
+            self.events = events
+        }.store(in: &cancellables)
+
+    }
+}
+
+struct VEvent: Decodable {
+    var id: Int
+    var city: String
+    var remaining: Int
+    var title: String
+}
--- VaccineFinder/ContentView.swift
+++ VaccineFinder/ContentView.swift
@@ -7,11 +7,63 @@
 
 import SwiftUI
 
+enum ListItem {
+    case date(String)
+    case title(String)
+    case event(VEvent)
+}
+
 struct ContentView: View {
+    @ObservedObject var state: AppState = AppState.shared
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("💉 Refresh 💉") {
+            state.load()
+        }.padding()
+        List(days(from: state.events), id: \.date) { day in
+            VStack {
+                Divider()
+                Text(day.date).font(.title)
+                Text(day.events.first?.title ?? "").font(.footnote).padding(5)
+                Group {
+                    ForEach(day.events, id: \.id) { event in
+                        Button(action: { openURL(URL(string: "https://healthevents.utahcounty.gov/event/\(event.id)")!) }) {
+                        HStack {
+                            Text(event.city)
+                            available(i: event.remaining)
+                            Spacer()
+                        }.padding(3)}
+                    }
+                }
+            }
+        }
+        .onAppear {
+            state.load()
+        }
     }
+}
+
+func available(i: Int) -> Text {
+    let color: Color
+    switch i {
+    case 1..<100: color = .orange
+    case 100...: color = .green
+    default: color = .red
+    }
+    
+    return Text("\(i)").foregroundColor(color)
+}
+
+struct Day {
+    var date: String
+    var events: [VEvent]
+}
+
+func days(from dict: [String: [VEvent]]) -> [Day] {
+    dict.sorted(by: { $0.key < $1.key })
+        .filter({ $0.value.count > 0 })
+        .map({ Day(date: $0.key, events: $0.value) })
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +71,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
