1e461bda43c0de4878a0225adcfb126ad6cb1798
Bart Whiteley <601312+bwhiteley@users.noreply.github.com>
2021-03-12 20:42:09 -0700

    register for push notifications

--- VaccineFinder/VaccineFinderApp.swift
+++ VaccineFinder/VaccineFinderApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct VaccineFinderApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }        
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        //Notifications
+        UIApplication.shared.registerForRemoteNotifications()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)          {
+        print("device token:", deviceToken.map { String(format: "%02.2hhx", $0) }.joined())
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
